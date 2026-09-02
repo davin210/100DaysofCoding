746b6ba0d57fbd115cc251f834ecf01839150711
davin210 <istiqoridavin@gmail.com>
2026-09-02 20:29:38 +0700

    Day1.java

---
+++ Day1.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int A = 8;
+        int B = 4;
+        // disini kita melaakukan input variabel A dan B
+        System.out.println(A + B);
+        /*disini kita menjumlahkan variabel A dan B */
+    }
+}
