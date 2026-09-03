4adedb93c7d133bdfd1a3d5de88cd12a561c3869
davin210 <istiqoridavin@gmail.com>
2026-09-03 15:49:32 +0700

    day3.java

--- day3.java
+++ day3.java
@@ -6,5 +6,11 @@ public class App {
         System.out.printf("Nama :%s\t",Nama);
         System.out.printf("Nama :%s\"",Nama);
         System.out.printf("Nama :%s\\",Nama);
+        /**
+         * \n membuat baris baru
+         * \t memberikan spasi selbar tab
+         * \" menampilkan tanda kutip ganda
+         * \\ menampilkan garis miring itu sendiri
+         */
     }
 }
