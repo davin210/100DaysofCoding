049b60fd0dbf27520eecf96958570d7b40211481
davin210 <istiqoridavin@gmail.com>
2026-09-03 16:11:50 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double no1 = 3.14159;
+        float no2 = 36.5f;
+        System.out.println("double :"+ no1);
+        System.out.println("float :"+ no2);
+    }
+}
