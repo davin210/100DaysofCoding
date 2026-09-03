58356b8f4f0daa2d7bf3dcf3703cf862dcf1e326
davin210 <istiqoridavin@gmail.com>
2026-09-03 15:44:13 +0700

    day3.java

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String Nama = "Davin";
+
+        System.out.printf("Nama :%s\n",Nama);
+        System.out.printf("Nama :%s\t",Nama);
+        System.out.printf("Nama :%s\"",Nama);
+        System.out.printf("Nama :%s\\",Nama);
+    }
+}
