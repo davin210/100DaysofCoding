337e617f9270beea8c7fdb8409ec6bad4e4bca6d
davin210 <istiqoridavin@gmail.com>
2026-09-05 08:45:56 +0700

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,18 @@
+public class day4 {
+
+    public static void main(String[] args) {
+        byte BYTE = 127;
+        short SHORT = 32767;
+        int INT = 2147483647;
+        long LONG = 922337203;
+        // tipe data ini adalah tipe data bilangan bulat
+        System.out.println(BYTE);
+        // byte mampu menampungn nilai hingga 8-bit
+        System.out.println(SHORT);
+        // short mampu menampung nilai hingga 16-bit
+        System.out.println(INT);
+        // int mampu menamoung nilai hingga 32-bit
+        System.out.println(LONG);
+        // long mampu menampung nilai hingga 64-bit
+    }
+}
