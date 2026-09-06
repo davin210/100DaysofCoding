badd30c785e20c8143fddc4e26c62d030b571e8e
davin210 <istiqoridavin@gmail.com>
2026-09-06 15:26:38 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public class day5 {
+    public static void main(String[] args) {
+        double DOUBLE = 75.5F;
+        float FLOAT = 3.14159F;
+        // tipe data ini adalah tipe data bilangan desimal
+        System.out.println(DOUBLE);
+        // double dapat menampung nilai hingga 32-bit 
+        System.out.println(FLOAT);
+        // float dapat menampung nilai hingga 64-bit
+    }
+}
