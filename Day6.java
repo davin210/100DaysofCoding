f19dc1811fa6293590dd2fb44c0ab5cddda867c1
davin210 <istiqoridavin@gmail.com>
2026-09-07 07:43:04 +0700

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class day6 {
+    public static void main(String[] args) {
+        char inisial_nama = 'D';
+        // char hanya dapat menampung 1 huruf saja dan mengunakan tanda '' untuk mengaktifkanya
+        boolean lampu_menyala = true;
+        // boolean adalah tipe data yang hanyaa memiliki 2 jawaban saja yaitu true atau false
+        System.out.println(inisial_nama);
+        System.out.println(lampu_menyala);
+    }
+}
