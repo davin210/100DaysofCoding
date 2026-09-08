2a1c6172d804d8852afc191fb596014c4678a99f
davin210 <istiqoridavin@gmail.com>
2026-09-08 16:45:17 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,7 @@
+public class day7 {
+    public static void main(String[] args) {
+        String Nama = "Davin Istiqori";
+        // string adalah tipe data yang menampilkan kalimat atau kata 
+        System.out.println(Nama);
+    }
+}
