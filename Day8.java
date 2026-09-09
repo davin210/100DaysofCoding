4d0b800046b887b1a09ffe15e00198c4691b21e7
davin210 <istiqoridavin@gmail.com>
2026-09-09 07:56:23 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,16 @@
+public class day8 {
+    public static void main(String[] args) {
+        int totalMahasiswa = 29;
+        /** cara mendeklarasikan adalah yang pertama kita harus tau
+         * data yang ingin kita input itu bertipe data apa angka kah, inisial atau kalimat
+         * lalu kita ketik tipe datanya
+         * nama dari data tersebut
+         * dan jumlahnya
+         */
+        totalMahasiswa = 30;
+        /**sedangkan untuk update data kita perlu menuliskan nama data tersebut 
+         * dan memasukan data yaang terbaru 
+         */
+        System.out.println(totalMahasiswa);
+    }
+}
