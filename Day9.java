2b3ea6449f196d31787c757e1ba4a07de6ddd695
davin210 <istiqoridavin@gmail.com>
2026-09-10 11:07:39 +0700

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class day9 {
+    public static void main(String[] args) {
+        final int jumlahMahasiswaLulus = 30;
+        /**final atau kostanta adalah perintah
+         * bahwa variabel tersebut tidak dapat diubah-ubah
+         * atau bisa dibilang data yang permanen
+         */
+        System.out.println(jumlahMahasiswaLulus);
+    }
+}
