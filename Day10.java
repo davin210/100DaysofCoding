1443a3cf80d2a78940988de74ccf90bd7cdf15ce
davin210 <istiqoridavin@gmail.com>
2026-09-11 16:17:23 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,30 @@
+public class day10 {
+    public static void main(String[] args) {
+        // tipe data bilangan bulat:
+       System.out.println("byet\t:"+Byte.MIN_VALUE+" sampai "+Byte.MAX_VALUE);
+        // byte memiliki kapasitas yaitu 8 bit
+        System.out.println("short\t:"+Short.MIN_VALUE+" sampai "+Short.MAX_VALUE);
+        // short memiliki kapasitas yaitu 16 bit
+        System.out.println("int\t:"+Integer.MIN_VALUE+" sampai "+Integer.MAX_VALUE);
+        // int memiliki kapasitas yaitu 32 bit
+        System.out.println("long\t:"+Long.MIN_VALUE+" sampai "+Long.MAX_VALUE);
+        // long memiliki kapasitas yaitu 64 bit
+        
+        // tipe data bilangan desimal:
+        System.out.println("float\t:"+Float.MIN_VALUE+" sampai "+Long.MAX_VALUE);
+        // float memiliki kapsitas yaitu 32 bit
+        System.out.println("double\t:"+Double.MIN_VALUE+" sampai "+Double.MAX_VALUE);
+        // double memiliki kapasitaas yaitu 64 bit
+
+    
+        char maksimal1 ='a';
+        // char memiliki kapasitas yaitu 1 karakter
+        boolean maksimal2 = true;
+        /**boolean hanya memiliki 2 keadaan yaitu true atau false atau suatu keadaan yang ditentu
+         * contoh 3<5
+         */
+        String maksimal3 = "tak terhingga";
+        // untuk String itu tak terhingga kapasitasnya tergantung dari memory dan keadaan
+        
+    }
+}
