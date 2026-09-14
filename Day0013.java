b5dddc4acae35918f5c12edec3c1969e3dbfc9aa
davin210 <istiqoridavin@gmail.com>
2026-09-14 21:50:15 +0700

    Create Day0013.java

---
+++ Day0013.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class evaluasi5 {
+    public static void main(String[] args) {
+        Scanner da =new Scanner(System.in);
+        System.out.println("===BIO DATA===");
+
+        System.out.print("Nama Lengkap :");
+        String namaLengkap =da.nextLine();
+        System.out.print("NIM:");
+        String nim =da.nextLine();
+        System.out.print("umur :");
+        int umur =da.nextInt();
+        System.out.print("tinggi Badan :");
+        double tinggi =da.nextDouble();
+        System.out.print("Jenis kelaminn :");
+        char jenis=da.next().charAt(0);
+
+        System.out.println("\t===BIODATA MAHASISWA===");
+        System.out.println("Nama  Lengkap \t:"+namaLengkap);
+        System.out.println("NIM \t\t:"+nim);
+        System.out.println("Umur \t\t:"+umur+" tahun");
+        System.out.println("Tinggi Badan \t:"+tinggi+"cm");
+        System.out.println("Jenis Kelamin \t:"+jenis);
+        da.close();
+    }
+} 
