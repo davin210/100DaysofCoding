aae677c00f15e10a9f10ed9f87406eac36d7b6cd
davin210 <istiqoridavin@gmail.com>
2026-09-15 07:30:16 +0700

    Create Day014.java

---
+++ Day014.java
@@ -0,0 +1,10 @@
+public class day14 {
+    public static void main(String[] args) {
+        int A = 23;
+        int B = 2;
+        int hasilPenjumlahan = A+B;
+        int hasilPengurangan = A-B;
+        System.out.println(hasilPenjumlahan);
+        System.out.println(hasilPengurangan);
+    }
+}
