6800b28f73681aa6e8fb36162930e1f64e71b7e9
davin210 <istiqoridavin@gmail.com>
2026-09-16 13:18:22 +0700

    Update Day014.java

--- Day014.java
+++ Day014.java
@@ -8,7 +8,6 @@ public class day14 {
         System.out.println(A-B);
         System.out.println(hasilPenjumlahan);
         System.out.println(hasilPengurangan);
-        // jika pembagian penyebutnya betnilai 0 maka hasil bagi akan tetpa 0 contohnya (0/6) maka hasilnya 0
-        // tetpai jika pembilang bernilai 0 maka kode akan eror contoh (10/0) kode akan eror
+        
     }
 }
