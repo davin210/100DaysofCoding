5aa9781fcb8acdd4b26612868f43999a0938b986
davin210 <istiqoridavin@gmail.com>
2026-09-16 10:28:45 +0700

    Create Day015.java

---
+++ Day015.java
@@ -0,0 +1,12 @@
+public class day15 {
+    public static void main(String[] args) {
+        int A = 55;
+        int B = 5;
+        int hasilPerkalian= A*B;
+        int hasilPembagian= A/B;
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(hasilPerkalian);
+        System.out.println(hasilPembagian);
+    }
+}
