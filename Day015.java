fdf33b5c14284ad7ec29d4322d25ecb13b0ad90b
davin210 <istiqoridavin@gmail.com>
2026-09-16 13:18:36 +0700

    Update Day015.java

--- Day015.java
+++ Day015.java
@@ -8,5 +8,7 @@ public class day15 {
         System.out.println(A/B);
         System.out.println(hasilPerkalian);
         System.out.println(hasilPembagian);
+        // jika pembagian penyebutnya betnilai 0 maka hasil bagi akan tetpa 0 contohnya (0/6) maka hasilnya 0
+        // tetpai jika pembilang bernilai 0 maka kode akan eror contoh (10/0) kode akan eror
     }
 }
