f8f41f1d0419399e6adf6847453f3f5a6aa284c8
davin210 <istiqoridavin@gmail.com>
2026-09-17 17:23:03 +0700

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,9 @@
+public class day16 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 3;
+        float hasil = A%B;
+        // moodulos adallah sisa dari pembagian yang tidak dapat dibagi
+        System.out.println(hasil);
+    }
+}
