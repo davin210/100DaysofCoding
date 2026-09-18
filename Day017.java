65a187dda06340a868b0a62ca29bc1d23c75fa8d
davin210 <istiqoridavin@gmail.com>
2026-09-18 16:28:05 +0700

    Create Day017.java

---
+++ Day017.java
@@ -0,0 +1,17 @@
+public class day17 {
+    public static void main(String[] args) {
+        int A = 34;
+        int B  = 3;
+
+        System.out.println(B+=A);
+        System.out.println();
+        System.out.println(B-=A);
+         System.out.println();
+        System.out.println(B*=A);
+         System.out.println();
+        System.out.println(B/=A);
+         System.out.println();
+        System.out.println(B%=A);
+        
+    }
+}
