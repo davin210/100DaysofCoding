ca3fe4178d6abd2bd75165fefa2ed8946c8a0b02
davin210 <istiqoridavin@gmail.com>
2026-09-24 19:42:32 +0700

    Update Day022.java

--- Day022.java
+++ Day022.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class day22 {
     public static void main(String[] args) {
-        int A = 34;
-        int B = 56;
+        Scanner d = new  Scanner(System.in);
 
-        System.out.println("sebelum ditukar: A="+A+"\tB="+B);
-        
+        System.out.print("masukkan nilai A:");
+        int A = d.nextInt();
+        System.out.print("Masukkan nilai B:");
+        int B = d.nextInt();
          int da = A;
         A = B;
         B = da;
-
-        System.out.println("sesudah ditukar: A="+A+"\tB="+B);
+        System.out.println("=== NILAI ANDA AKAN KAMI TUKAR ===");
+        System.out.println("Nilai A:"+A);
+        System.out.println("Nilai B:"+B);
+        d.close();
     }
 }
