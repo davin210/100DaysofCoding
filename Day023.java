ba35f7c6472c0e3b145d9fb92882da23c8f1347f
davin210 <istiqoridavin@gmail.com>
2026-09-24 18:21:15 +0700

    Create Day023.java

---
+++ Day023.java
@@ -0,0 +1,10 @@
+public class day23 {
+    public static void main(String[] args) {
+        int sisi1 = 32;
+        int sisi2 = 8;
+
+        int luasPersegi= sisi1*sisi2;
+
+        System.out.println("luas persegi ="+luasPersegi);
+    }
+}
