ad450ba7e09a6f6afdc65a5218f3ba7b0bad653a
davin210 <istiqoridavin@gmail.com>
2026-09-24 19:32:38 +0700

    Update Day023.java

--- Day023.java
+++ Day023.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class day23 {
     public static void main(String[] args) {
-        int sisi1 = 32;
-        int sisi2 = 8;
-
-        int luasPersegi= sisi1*sisi2;
-
-        System.out.println("luas persegi ="+luasPersegi);
+        Scanner d= new Scanner(System.in);
+        System.out.print("Masukkin sisi1 :");
+        int sisi1 = d.nextInt();
+        System.out.print("Masukkin sisi2 :");
+        int sisi2 = d.nextInt();
+        System.out.println("luas persegi ="+(sisi1*sisi2));
+        d.close();
     }
 }
